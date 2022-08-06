f2ff3e3b862bd49e3eaba142e69566e8b6f7bf39
Cristal Flynn <94497598+cristalflynn@users.noreply.github.com>
2022-08-05 23:16:47 -0500

    OG file

---
+++ math_module.py
@@ -0,0 +1,13 @@
+from math import pi
+
+
+def squarefootage(length, width):
+    return length * width
+
+#print(squarefootage(5,10))
+
+def circumference(radius):
+    return 2 * pi * radius
+
+#print(circumference(5)) 
+
